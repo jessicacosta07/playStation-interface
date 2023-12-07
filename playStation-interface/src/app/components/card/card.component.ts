0fd6a47d6349ea6692f8e9aa321164e7b23a079a
jessicacosta07 <jessikacostajf@gmail.com>
2023-12-07 13:36:51 -0300

    ajustes finais

--- playStation-interface/src/app/components/card/card.component.ts
+++ playStation-interface/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { CardLabelComponent } from "../card-label/card-label.component";
 import { CardPricingComponent } from "../card-pricing/card-pricing.component";
 
@@ -10,4 +10,18 @@ import { CardPricingComponent } from "../card-pricing/card-pricing.component";
     imports: [CardLabelComponent, CardPricingComponent]
 })
 export class CardComponent {
+    @Input()
+    gameOver: string = ""
+    @Input()
+    gameLabel: string = ""
+    @Input()
+    gameType: string = "Digital PS4"
+    @Input()
+    gamePricing: string = "R$ 129,99"
+
+    constructor() { }
+    
+    ngOnInit(): void{
+
+    }
 }
